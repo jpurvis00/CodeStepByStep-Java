e2768e5d8f89c5a8835a496ef6643c9a9173d2e8
jpurvis <jpurvis@JP.DoubleStar.local>
2019-03-12 11:27:56 -0400

    Solution for: https://codestepbystep.com/problem/view/java/fileio/flipAndReverseLines

---
+++ src/fileIO/flipAndReverseLines.java
@@ -0,0 +1,82 @@
+/* Read lines from a file.  Flip alternating lines and flip case on the lines.
+ * First line should be all caps.  Second line should be all lower case.  Also
+ * reverse the string.   
+ */
+
+package fileIO;
+
+import java.io.*;
+import java.util.*;
+
+public class flipAndReverseLines {
+
+	public static void main(String[] args) {
+		/* Opening the scanner and file outside of the try loop allows us to access the file 
+		 * var so that we can ref it in our catch loop.
+		 */
+		while(true) {
+			System.out.print("Enter file name: ");
+			Scanner input = new Scanner(System.in);
+			File file = new File(input.nextLine());
+			int totLines = 0;
+			
+			try {
+				input = new Scanner(file);
+				totLines = flipLines1(input);
+				input.close();
+				System.out.println("Total # of lines: " + totLines);
+				break;
+			}
+			catch(IOException ex) {
+				System.out.println("Unable to open input file " + file);
+			}
+		}
+	}
+	
+	/* Gets two lines from the file at a time and flips them.  First line 
+	 * is converted to all caps and 2nd line is converted to all lower case.
+	 * This is done for every other line that exists.  
+	 * Pre:  File is passed into method
+	 * Post: All new lines have been printed to output
+	 */
+	private static int flipLines1(Scanner input) {
+		String line1 = "";
+		String line2 = "";
+		int totLines = 0;
+		
+		while(input.hasNextLine()) {
+			line1 = input.nextLine().toLowerCase();
+			line1 = reverseString(line1);
+			totLines++;
+						
+			//Even though we are in the loop bc there is a next line, there is no 
+			//guarantee that there are 2 more lines. Only assign and print if there is a 2nd line.
+			if(input.hasNextLine()) {
+				line2 = input.nextLine().toUpperCase();
+				line2 = reverseString(line2);
+				System.out.println(line2);
+				totLines++;
+			} else
+				//# of lines is odd, last line of odd should always be uppercase
+				line1 = line1.toUpperCase();
+			
+			System.out.println(line1);
+		}
+		return totLines;
+	}
+	
+	/* Reverses the order of the string.
+	 * ex. hi there -> ereht ih
+	 * Pre:  1 string is passed in
+	 * Post: String has been reversed and returned
+	 */
+	private static String reverseString(String line) {
+		String str = "";
+				
+		for(int i = line.length() - 1; i >= 0; i--) {
+			str += line.charAt(i);
+		}
+				
+		return str;
+	}
+}
