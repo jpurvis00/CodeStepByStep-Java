2ed9c34958d96cdd974217b83f789c2a54030d37
jpurvis <jpurvis@JP.DoubleStar.local>
2019-03-15 14:10:22 -0400

    Solution for: https://codestepbystep.com/problem/view/java/arrays/squaredArray

--- src/Arrays/squaredArray.java
+++ src/Arrays/squaredArray.java
@@ -1,5 +1,20 @@
+/* Write a variable declaration and for loop necessary to create 
+ * and initialize an integer array named squares that contains the following values:
+ * 0 1 4 9 16 25 36 49 64 81 100
+ */
+
 package Arrays;
 
 public class squaredArray {
-
+	public static void main(String[] args) {
+		int[] intArr = new int[11];
+		
+		for(int i = 0; i < 11; i++) {
+			intArr[i] = i * i;
+		}
+		
+		for(int i = 0; i < intArr.length; i++) {
+			System.out.print(intArr[i] + " ");
+		}
+	}
 }
